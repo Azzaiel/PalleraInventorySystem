6acbbcb3801da4b40f05da0518535c9d836be695
BananaSadSign <markjeromeb@yahoo.com>
2014-01-25 13:14:05 +0800

    updated db

---
+++ DB Dump/palleras_inventory_suppliers.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE  IF NOT EXISTS `palleras_inventory` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `palleras_inventory`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: palleras_inventory
+-- ------------------------------------------------------
+-- Server version	5.5.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `suppliers`
+--
+
+DROP TABLE IF EXISTS `suppliers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suppliers` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) DEFAULT NULL,
+  `SALES_CONTACT` varchar(255) DEFAULT NULL,
+  `SALES_EMAIL` varchar(50) DEFAULT NULL,
+  `SALES_PHONE_NUMBER` varchar(50) DEFAULT NULL,
+  `COMPANY_ADDRESS` varchar(255) DEFAULT NULL,
+  `COMPANY_PHONE_NUMBER` varchar(45) DEFAULT NULL,
+  `CREATED_BY` varchar(100) DEFAULT NULL,
+  `CREATED_DATE` datetime DEFAULT NULL,
+  `LAST_MOD_DATE` datetime DEFAULT NULL,
+  `LAST_MOD_BY` varchar(100) DEFAULT NULL,
+  `ACTIVE` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+LOCK TABLES `suppliers` WRITE;
+/*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
+INSERT INTO `suppliers` VALUES (1,'Metro Marketing','asd','dasdsdsd','dasd','asdasdas','asdas','System','2014-01-11 11:52:34','2014-01-11 11:52:34','System','Y'),(2,'Davis Paint','asd','dasdsdsd','dasd','asdasdas','asdas','System','2014-01-11 11:49:47','2014-01-11 12:01:54','System','N'),(3,'Mayland Constraction','Richard Reyles','asdasdasd','asdasdas','Some where','sdsdsd','System','2014-01-11 12:11:14','2014-01-11 12:11:14','System','Y');
+/*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-01-24 21:12:03
